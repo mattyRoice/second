ac1dd0a057558800cf6d65eedf5c8b8a4e37c703
DESKTOP-346MTU5\kyu1148 <kyu1148@gmail.com>
2022-04-08 14:09:14 +0900

    Ex01.java > while 문으로 반복하도록 수정

--- web_3/src/main/java/etc/Ex01.java
+++ web_3/src/main/java/etc/Ex01.java
@@ -3,12 +3,13 @@ package etc;
 public class Ex01 {
 	public static void main(String[] args) {
 		
-		for(int i=0; i<10; i++) {
+		int count = 1;
+		
+		while(count <= 10) {
+			count++;
+			
 			System.out.println("Hello World~!");
 		}
-		
-		
-		
-		
+				
 	}
 }
